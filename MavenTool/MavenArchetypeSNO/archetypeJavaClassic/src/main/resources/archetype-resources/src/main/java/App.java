e35b5e9b8d29ebdeff37a56d2bd8ae1b64116ad1
snonov <sebastien.normand@novencia.com>
2016-03-29 14:36:34 +0200

    init maven archetype part

---
+++ MavenTool/MavenArchetypeSNO/archetypeJavaClassic/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,24 @@
+package com.sno.testit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		sayHello();
+	}
+
+	public static void sayHello() {
+		LOGGER.info("Hello World!");
+	}
+	
+	
+	
+}
---
+++ MavenTool/MavenArchetypeSNO/archetypeJavaClassic/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+package com.sno.testit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void sayHello() {
+		Assert.assertTrue(true);
+		App.sayHello();
+	}
+}
